3cdb5305d7e4fa0a3abf2599b515276ef4df16bf
Petar Shomov <petar@activitystream.com>
2016-12-23 12:20:13 +0000

    ref

--- tests/overrides.js
+++ tests/overrides.js
@@ -1,19 +1,21 @@
 var path = require('path');
 var expect = require('chai').expect;
-var resolve = p => path.resolve(__dirname, 'merge-testset', p);
-var merge = require('../merge').merge;
+var resolve = p => path.resolve(__dirname, 'overrides-testset', p);
+var overrides = require('../overrides');
 
-describe.only('merging layers', function () {
+describe('file overrides', function () {
     it('file from base layer goes through to result', function () {
-        var result = merge(resolve('base-layer-pass-through'), ['top', 'base']);
+        var result = overrides(resolve('base-layer-pass-through'), ['top', 'base']);
         expect(result).to.deep.equals([{ file: 'a.json', layers: ['base'] }, { file: 'b.js', layers: ['base'] }]);
     })
+    
     it('file from top layer overrides base layer', function () {
-        var result = merge(resolve('simple-override'), ['top', 'base']);
+        var result = overrides(resolve('simple-override'), ['top', 'base']);
         expect(result).to.deep.equals([{ file: 'a.json', layers: ['base', 'top'] }, { file: 'b.js', layers: ['base'] }]);
     })
+
     it('file from top layer with no base layer revision', function () {
-        var result = merge(resolve('no-override'), ['top', 'base']);
+        var result = overrides(resolve('no-override'), ['top', 'base']);
         expect(result).to.deep.equals([{ file: 'b.js', layers: ['base'] }, { file: 'a.json', layers: ['top'] }]);
     })
 })
