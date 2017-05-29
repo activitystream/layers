b1f5f3ca82a91705e836e6c9b47058aeb4329166
Gudmundur Orn Johannsson <gudmundur.orn@gmail.com>
2017-05-29 08:53:20 +0000

    It should merge subtrees in an additive way, so that subkeys don't disappear.

--- merge.js
+++ merge.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var traverse = require('traverse');
+var merge = require('deepmerge');
 var tmp = require('tmp');
 var debug = require('debug')('merge');
 var mkdirp = require('mkdirp');
@@ -21,25 +22,19 @@ var ops = {
     }
 }
 
-var mergeJSON = function (base, json) {
-    var traversedBase = traverse(base);
-    traverse(json).forEach(function (el) {
-        var add = el.add;
-        var remove = el.remove;
-        if (add || remove) {
-            this.update(ops.list(traversedBase.get(this.path), el));
-        }
-    })
-    return Object.assign(base, json);
-}
-
 var mergeFiles = function (fileOverrides, target) {
     var resultContent = fileOverrides.reduce((acc, o) => {
-        switch (path.extname(o)) {
-            case '.json':
-                return mergeJSON(acc, JSON.parse(fs.readFileSync(o, 'utf8')));
-            case '.js':
-                return require(o)(acc, ops);
+        try {
+            switch (path.extname(o)) {
+                case '.json':
+                    return merge(acc, JSON.parse(fs.readFileSync(o, 'utf8')));
+                case '.js':
+                    return require(o)(acc, ops);
+            }
+        } catch (ex) {
+            console.log("acc=" + JSON.stringify(acc));
+            console.log("o=" + JSON.stringify(JSON.parse(fs.readFileSync(o, 'utf8'))));
+            throw ex;
         }
 
     }, {});
--- tests/merge.js
+++ tests/merge.js
@@ -49,36 +49,4 @@ describe('file merge', function () {
             expect(resultContent).to.deep.equals({ a: ['b'] });
         })
     })
-    describe('JSON content manipulations', function () {
-        it('array items addition', function () {
-            mergeFiles([resolve('array.json'), resolve('add-to-array.json')], result);
-            var resultContent = JSON.parse(fs.readFileSync(result, 'utf8'));
-            expect(resultContent).to.deep.equals({ a: ['a', 'b'] });
-        })
-
-        it('array items removal', function () {
-            mergeFiles([resolve('array.json'), resolve('remove-from-array.json')], result);
-            var resultContent = JSON.parse(fs.readFileSync(result, 'utf8'));
-            expect(resultContent).to.deep.equals({ a: [] });
-        })
-
-        it('array items addition and removal', function () {
-            mergeFiles([resolve('array.json'), resolve('add-and-remove-array.json')], result);
-            var resultContent = JSON.parse(fs.readFileSync(result, 'utf8'));
-            expect(resultContent).to.deep.equals({ a: ['b'] });
-        })
-    })
-})
-
-describe('merge tree', function(){
-    var resolve = p => path.resolve(__dirname, 'mergetree-testset', p);
-    it('should layers of trees', function(){
-        var output = mergeDirectory(resolve('base-layer-pass-through'), 'top')
-
-        var resultContent = JSON.parse(fs.readFileSync(path.resolve(output, 'a.json'), 'utf8'));
-        expect(resultContent).to.deep.equals({ a: 1 });
-        resultContent = JSON.parse(fs.readFileSync(path.resolve(output, 'b.json'), 'utf8'));
-        expect(resultContent).to.deep.equals({ b: true });
-        
-    })
-})
+});
